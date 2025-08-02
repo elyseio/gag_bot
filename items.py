b88438df99ae2af5ac93dca96644ade6e558c749
elyseio <123453529+elyseio@users.noreply.github.com>
2025-08-02 16:09:51 +0800

    stored grow a garden sellable items into a dictionary

---
+++ items.py
@@ -0,0 +1,28 @@
+gear_items = {
+    "shop": {
+        0: "Watering Can",
+        1: "Trading Ticket",
+        2: "Trowel",
+        3: "Recall Wrench",
+        4: "Basic Sprinkler",
+        5: "Advanced Sprinkler",
+        6: "Medium Toy",
+        7: "Medium Treat",
+        8: "Godly Sprinkler",
+        9: "Magnifying Glass",
+        10: "Master Sprinkler",
+        12: "Cleaning Spray",
+        13: "Favorite Tool",
+        14: "Harvest Tool",
+        15: "Friendship Pot",
+        16: "Levelup Lollipop"
+    },
+    "egg": {
+        0: "Common Egg",
+        1: "Common Summer Egg",
+        2: "Rare Summer Egg",
+        3: "Mythical Egg",
+        4: "Paradise Egg",
+        5: "Bug Egg"
+    }
+}
